00a7ae79917c16d6596de5efb1c9d653fb4a1407
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-20 08:34:04 -0700

    adding push and pull code

--- monty.h
+++ monty.h
@@ -21,8 +21,22 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
-/*------ Declaring head/top of stack -------*/
-stack_t *top = 0;
+/*===========================================*/
+/*============  Opcode Functions ============*/
+/*===========================================*/
+
+int main(int ac, char **av);
+
+int monty_run(FILE *fd);
+char **tokening(char *line, char *delim);
+int empty_line(char *line, char *delims);
+
+int execute(char **token, stack_t **stack, unsigned int line_num);
+int monty_push(stack_t **stack, char **token, unsigned int line_num);
+void monty_pall(stack_t **stack, unsigned int line_num);
+
+
+
 
 
 /**
--- opcode.c
+++ opcode.c
@@ -1,9 +1,61 @@
 #include "monty.h"
 
-void push(int x)
-{
-	stack_t *newnode;
-	newnode = malloc(sizeof(stack_t));
+/**
+ * monty_push - A function that insert an element to stack
+ * @stack: pointer to stack struct
+ * @token: pointer to command
+ * @line_num: number of line
+ * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure
+ */
 
-	if (new_node == NULL)
-		fprintf(stderr, "Error: malloc failed");
+int monty_push(stack_t **stack, char **token, unsigned int line_num)
+{
+	stack_t *new;
+	int i = 0;
+
+	if (token[1] == NULL)
+		return (f_errors(0, line_num));
+	while (token[1][i])
+	{
+		if (token[1][i] == '-' && i == 0)
+		{
+			i++;
+			continue;
+		}
+		if (token[1][i] < '0' || token[1][i] > '9')
+		{
+			free_stack(stack);
+			return (f_errors(0, line_num));
+		}
+		i++;
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (usage_error(0));
+
+	new->n = atoi(token[1]);
+	if ((*stack) != NULL)
+		(*stack)->prev = new;
+	new->next = *stack;
+	new->prev = NULL;
+	*stack = new;
+	return (EXIT_SUCCESS);
+}
+/**
+ * monty_pall - a function that prints stack data
+ * @stack: pointer to top of stack_t
+ * @line_num: number of line
+ * Return: Nothing
+ */
+void monty_pall(stack_t **stack, unsigned int line_num)
+{
+	stack_t *tmp = *stack;
+	(void)line_num;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
