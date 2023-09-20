0831981a7aa437780075ea41f654c0b592b965b6
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-20 15:48:05 -0700

    fixing some errors

--- monty.h
+++ monty.h
@@ -58,8 +58,8 @@ void _sub(stack_t **stack, unsigned int line_num);
 void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
-void monty_pstr(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
 
 
 
